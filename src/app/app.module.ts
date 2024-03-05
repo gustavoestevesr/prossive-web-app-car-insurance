a13718029be74dcf75ec880d6da227222c4d6e78
gustavoesteves <gustavoesteves@directon.com.br>
2024-03-05 18:20:21 -0300

    feat: adding new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,37 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { MenuComponent } from './components/menu/menu.component';
 import { CadastroSeguroComponent } from './components/cadastro-seguro/cadastro-seguro.component';
 import { ListarSeguroComponent } from './components/listar-seguro/listar-seguro.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     CadastroSeguroComponent,
-    ListarSeguroComponent
+    ListarSeguroComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
