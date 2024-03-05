e3bad9bc0054cd3f34fc1095caa1b87978ee01bc
gustavoesteves <gustavoesteves@directon.com.br>
2024-03-05 20:57:50 -0300

    feat: register ensurance cars with options of brands car

--- src/app/components/cadastro-seguro/cadastro-seguro.component.ts
+++ src/app/components/cadastro-seguro/cadastro-seguro.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICarro } from 'src/app/models/ICarro';
+import { IProprietario } from 'src/app/models/IProprietario';
+import { SeguroModel } from 'src/app/models/SeguroModel';
+import { CarroService } from './../../services/carro.service';
 
 @Component({
   selector: 'app-cadastro-seguro',
   templateUrl: './cadastro-seguro.component.html',
-  styleUrls: ['./cadastro-seguro.component.scss']
+  styleUrls: ['./cadastro-seguro.component.scss'],
 })
-export class CadastroSeguroComponent {
+export class CadastroSeguroComponent implements OnInit {
+  public seguro = new SeguroModel({} as ICarro, {} as IProprietario);
+  public marcasCarros$!: Observable<ICarro[]>;
 
+  constructor(private carroService: CarroService) {}
+
+  ngOnInit(): void {
+    this.marcasCarros$ = this.carroService.getMarcas();
+    console.log(this.marcasCarros$);
+  }
+
+  adicionar() {}
+
+  enviarNotificacao() {}
 }
