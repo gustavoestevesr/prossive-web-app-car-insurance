59c45556b9a891cdd499ed3fe5f72f3469a336ab
gustavoesteves <gustavoesteves@directon.com.br>
2024-03-05 18:05:03 -0300

    feat: routing for pages and wrong paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroSeguroComponent } from './components/cadastro-seguro/cadastro-seguro.component';
+import { ListarSeguroComponent } from './components/listar-seguro/listar-seguro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cadastro-seguro' },
+  { path: 'cadastro-seguro', component: CadastroSeguroComponent},
+  { path: 'listar-seguro', component: ListarSeguroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
