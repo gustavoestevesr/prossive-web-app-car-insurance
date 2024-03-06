95d79ac1cabdc952fb9f872ca029cf7fad1b084b
gustavoesteves <gustavoesteves@directon.com.br>
2024-03-05 21:01:23 -0300

    feat: listing insurances of cars

--- src/app/components/listar-seguro/listar-seguro.component.ts
+++ src/app/components/listar-seguro/listar-seguro.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SeguroModel } from 'src/app/models/SeguroModel';
+import { SeguroService } from 'src/app/services/seguro.service';
 
 @Component({
   selector: 'app-listar-seguro',
   templateUrl: './listar-seguro.component.html',
   styleUrls: ['./listar-seguro.component.scss']
 })
-export class ListarSeguroComponent {
+export class ListarSeguroComponent implements OnInit {
+  public seguros$!: Observable<SeguroModel[]>;
 
+  constructor(private seguroService: SeguroService) { }
+
+  ngOnInit() {
+    this.seguros$ = this.seguroService.listar();
+  }
 }
